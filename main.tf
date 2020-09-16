cbc720a6ce721805725742831412be480886d082
Kevin Head <khead@redventures.com>
2020-09-16 14:04:06 -0500

    add additional module

--- main.tf
+++ main.tf
@@ -7,7 +7,10 @@ module "vpc_explicit" {
   source  = "registry.development-stack.com/terraform-aws-modules/vpc/aws"
   version = "2.46.0"
 }
-
+module "security-group" {
+  source  = "registry.development-stack.com/terraform-aws-modules/security-group/aws"
+  version = "~> 3.0"
+}
 # module "vpc_error" {
 #   source  = "registry.development-stack.com/terraform-aws-modules/vpc/aws"
 #   version = "~> 5.0"
